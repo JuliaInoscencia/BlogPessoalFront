4123e11945e859421c35a6e4766bd357926e580a
Júlia Inoscência <santos.julia@usp.br>
2022-03-14 22:02:37 -0300

    Blog Pessoal [Task: add APIs]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
+
 
 
 @NgModule({
@@ -16,12 +20,15 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     MenuComponent,
     RodapeComponent,
     EntrarComponent,
-    CadastroComponent
+    CadastroComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
+  user: Usuario = new Usuario();
+  confirmSenha: string;
+  tipoUser: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0, 0);
   }
+  confirmaSenha(event: any) {
+    this.confirmSenha = event.target.value;
+  }
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value;
+  }
+  cadastrar() {
+    this.user.tipo = this.tipoUser;
 
+    if (this.user.senha != this.confirmSenha) {
+      alert('As senhas divergem!');
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: Usuario) => {
+        this.user = resp
+        this.router.navigate(["/entrar"])
+        alert('Usuário cadastrado com sucesso!');
+      });
+    }
+  }
 }
